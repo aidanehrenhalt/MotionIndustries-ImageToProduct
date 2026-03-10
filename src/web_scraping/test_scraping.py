124a99bd945be4a34c32247c68ba6652e8d92fd2
Aceaid <aehrenhalt3@gatech.edu>
2026-03-10 08:07:59 -0400

    Housekeeping - removing venv/ from repo, updated requirements.txt, removing test_scraper.py

--- src/web_scraping/test_scraping.py
+++
@@ -1,7 +0,0 @@
-# DEPRECEATED - Used for testing, no longer needed after development of web_scraper.py, possibily using Selenium later on
-from selenium import webdriver
-
-# Path for Scraped Files
-PATH = "/home/aceaid/ECE4013/MotionIndustries-ImageToProduct/data/raw/"
-
-wd = webdriver.Chrome(PATH)
